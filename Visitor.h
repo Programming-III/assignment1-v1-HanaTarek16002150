8f5d507518042974efae0dabac926c579772db53
HanaTarek16002150 <hanatarekfaysel@gmail.com>
2025-11-12 15:47:05 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -6,7 +6,18 @@ using namespace std;
 
 //define visitor class here
 
-
+#ifndef visitor_h_included
+#define visitor_h_included
+#include<iostream>
+using namespace std;
+class visitor{
+    string vname;
+    int tickets;
+    public:
+    void displayInfo();
+    visitor(string vname,int tickets);
+};
+#endif
 
 
 
