b327eae60d9c6dd14d5c492eefe13d572b165ad6
HanaTarek16002150 <hanatarekfaysel@gmail.com>
2025-11-12 15:21:06 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -5,7 +5,19 @@
 
 //define mammal class here
 
-
+#ifndef mammal_h_included
+#define mammal_h_included
+#include<iostream>
+using namespace std;
+class Mammal:public animal{
+     string furcolor;
+     public:
+    Mammal(string name,int age,bool isHungry, string furcolor):animal(name,age,isHungry);
+    string getfuncolor();
+    void getfuncolor(string f);
+    
+};
+#endif
 
 
 
