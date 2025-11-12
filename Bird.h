f6b309f1731b930723e66e0682baf624a6b40d37
HanaTarek16002150 <hanatarekfaysel@gmail.com>
2025-11-12 15:24:27 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,8 +1,3 @@
-#ifndef BIRD_H
-#define BIRD_H
-
-#include "Animal.h"
-
 #ifndef bird_h_included
 #define bird_h_included
 #include<iostream>
@@ -11,6 +6,8 @@ class bird:public animal{
     float wingspan;
     public:
     bird(string name,int age,bool isHungry,float wingspan):animal(name,age,isHungry);
+    float getwingspan();
+    void setwingspan(float w );
 };
 #endif
 
