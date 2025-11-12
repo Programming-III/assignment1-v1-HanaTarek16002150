999771c8c1e9229861a56f70595055ef66aa5bfa
HanaTarek16002150 <hanatarekfaysel@gmail.com>
2025-11-12 15:15:43 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,7 +4,24 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+#ifndef animal_h_included
+#define animal_h_included
+#include <iostream>
+using namespace std;
+class animal{
+    string name;
+    int age;
+    bool isHungry;
+    public:
+    animal(string name,int age,bool isHungry);
+    void display();
+    void feed();
+    string getname();
+    int getage();
+    void setname(string n);
+    void setage(int a);
+    };
+    #endif
 
 
 
